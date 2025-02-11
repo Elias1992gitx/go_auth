3608c4c7d2cb86fcf540f223974882abbd776376
Elias Chane <eliashailu492@gmail.com>
2025-02-11 18:05:38 +0300

    login functionality

--- controllers/authController.go
+++ controllers/authController.go
@@ -6,16 +6,22 @@ import (
     "github.com/gofiber/fiber/v2"
     "golang.org/x/crypto/bcrypt"
     "fmt"
+    "github.com/golang-jwt/jwt/v5"  // Add this import
+    "time"      // Add this import
+    "strconv"   // Add this import
 )
 
+const secretKey = "jK6DX#mP9$vL2@nQ8wR4tY7hC3bE5sA"
 // Hello returns a simple "Hello world!!" message
 func Hello(c *fiber.Ctx) error {
     return c.SendString("Hello world!!")
 }
 
+//register function
 func Register(c *fiber.Ctx) error {
     fmt.Println("Received a registration request")
 
+    //Parses the incoming JSON payload into a map[string]string
     var data map[string]string
     if err := c.BodyParser(&data); err != nil {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -23,6 +29,7 @@ func Register(c *fiber.Ctx) error {
         })
     }
 
+    //Check for Existing User
     var existingUser models.User
     if err := database.DB.Where("email = ?", data["email"]).First(&existingUser).Error; err == nil {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -30,6 +37,7 @@ func Register(c *fiber.Ctx) error {
         })
     }
 
+    //Hash the Password using bcrypt library
     hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data["password"]), bcrypt.DefaultCost)
     if err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -37,20 +45,83 @@ func Register(c *fiber.Ctx) error {
         })
     }
 
+    //Creates a new models.User instance with the parsed data and hashed password
     user := models.User{
         Name:     data["name"],
         Email:    data["email"],
         Password: hashedPassword,
     }
-
     if err := database.DB.Create(&user).Error; err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
             "error": "Failed to create user",
         })
     }
 
+    //Return Success Response
     return c.JSON(fiber.Map{
         "message": "User created successfully",
     })
 }
 
+
+//login function
+func Login(c *fiber.Ctx) error {
+    fmt.Println("Received a login request")
+
+     // Parse request body
+    var data map[string]string
+    if err := c.BodyParser(&data); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "failed to parse request body",
+        })
+    }
+
+    // Check if user exists
+    var user models.User
+    database.DB.Where("email = ?", data["email"]).First(&user)
+    if user.ID == 0 {
+        fmt.Println("User not found")
+        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "message": "Invalid credentials",
+        })
+    }
+
+    // Compare passwords
+    err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"]))
+    if err != nil {
+        fmt.Println("Invalid Password:", err)
+        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "message": "Invalid credentials",
+        })
+    }
+
+     // Generate JWT token
+     claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "sub": strconv.Itoa(int(user.ID)), 
+        "exp": time.Now().Add(time.Hour * 24).Unix(), // Expires in 24 hours
+    })
+    token, err := claims.SignedString([]byte(secretKey))
+    if err != nil {
+        fmt.Println("Error generating token:", err)
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error": "Failed to generate token",
+        })
+    }
+
+    // Set JWT token in cookie
+    cookie := fiber.Cookie{
+        Name:     "jwt",
+        Value:    token,
+        Expires:  time.Now().Add(time.Hour * 24), // Expires in 24 hours
+        HTTPOnly: true,
+        Secure:   true,
+    }
+    c.Cookie(&cookie)
+
+
+     // Authentication successful, return success response
+     return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
+        "message": "Login successful",
+    })
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -8,4 +8,5 @@ import (
 func SetUpRoutes(app *fiber.App){
 	app.Get("/", controllers.Hello)
 	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
 }
