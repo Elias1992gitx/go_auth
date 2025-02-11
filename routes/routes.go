18e39b2c634e4594ce74f5ec2142880f0a88ac03
Elias Chane <eliashailu492@gmail.com>
2025-02-11 16:56:55 +0300

    user registration

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"JWT-Authentication-go/controllers"
+    "github.com/gofiber/fiber/v2"
+    "JWT-Authentication-go/controllers"
 )
 
 func SetUpRoutes(app *fiber.App){
 	app.Get("/", controllers.Hello)
+	app.Post("/api/register", controllers.Register)
 }
